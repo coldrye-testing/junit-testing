f505b08d9d1483a7ed3eeec2c7f5ceb967e4cbba
Carsten Klein <trancesilken@gmail.com>
2018-02-09 22:27:59 +0100

    fix junit-env-extension integration test

--- junit-env-extension/src/it/sample-test-project/src/test/java/eu/coldrye/junit5/env/samples/SampleTest.java
+++ junit-env-extension/src/it/sample-test-project/src/test/java/eu/coldrye/junit5/env/samples/SampleTest.java
@@ -1,8 +1,8 @@
 package eu.coldrye.junit5.env.samples;
 
-import eu.coldrye.junit5.env.EnvExtension;
-import eu.coldrye.junit5.env.EnvProvided;
-import eu.coldrye.junit5.env.Environment;
+import eu.coldrye.junit.env.EnvExtension;
+import eu.coldrye.junit.env.EnvProvided;
+import eu.coldrye.junit.env.Environment;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
