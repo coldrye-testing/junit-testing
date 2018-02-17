56b906ca0eebbbf18396ead88338c643ca1c0c4d
Carsten Klein <trancesilken@gmail.com>
2018-02-17 23:54:41 +0100

    fix javadoc issues

--- junit-env-extension/src/main/java/eu/coldrye/junit/env/EnvPhase.java
+++ junit-env-extension/src/main/java/eu/coldrye/junit/env/EnvPhase.java
@@ -18,8 +18,8 @@ package eu.coldrye.junit.env;
 
 /**
  * The enum EnvPhase models a means to control the behaviour of implementations of {@link EnvProvider} during the
- * various phases of both a {@link org.junit.platform.launcher.TestPlan}S execution and the execution of
- * {@link org.junit.jupiter.api.TestInstance}S.
+ * various phases of both a {@link org.junit.platform.launcher.TestPlan TestPlan}'s execution and the preparation and
+ * execution of individual {@link org.junit.jupiter.api.TestInstance TestInstance}S.
  *
  * @since 1.0.0
  */
@@ -28,19 +28,19 @@ public enum EnvPhase {
   /**
    * During initialization, i.e. before any tests are being run and also before any test instances will be prepared,
    * {@code EnvProvider}S are instructed to set up their provided environments by a call to
-   * {@link EnvProvider#setUpEnvironment(EnvPhase, java.util.Optional< java.lang.reflect.AnnotatedElement >)}.
+   * {@link EnvProvider#setUpEnvironment(EnvPhase, java.util.Optional) EnvProvider#setUpEnvironment()}.
    */
   INIT,
 
   /**
    * During shutdown, i.e. all tests have been run, {@code EnvProvider}S are instructed to tear down
-   * their provided environments by a call to {@link EnvProvider#tearDownEnvironment(EnvPhase, java.util.Optional< java.lang.reflect.AnnotatedElement >)}.
+   * their provided environments by a call to {@link EnvProvider#tearDownEnvironment(EnvPhase, java.util.Optional) EnvProvider#tearDownEnvironment()}.
    */
   DEINIT,
 
   /**
    * Before all tests in a given test class, {@code EnvProvider}S are instructed to set up their provided environments
-   * by a call to {@code EnvProvider#setUpEnvironment(EnvPhase, Optional<AnnotatedElement>)}.
+   * by a call to {@code EnvProvider#setUpEnvironment(EnvPhase, Optional)}.
    * <p>
    * It is up to the implementer of the {@code EnvProvider} to decide whether to set up all or only parts of the
    * environment.
@@ -49,7 +49,7 @@ public enum EnvPhase {
 
   /**
    * After all tests in a given test class, {@code EnvProvider}S are instructed to tear down their
-   * provided environments by a call to {@code EnvProvider#tearDownEnvironment(EnvPhase, Optional<AnnotatedElement>)}.
+   * provided environments by a call to {@code EnvProvider#tearDownEnvironment(EnvPhase, Optional)}.
    * <p>
    * It is up to the implementer of the {@code EnvProvider} to decide whether to tear down all
    * or only parts of the environment.
@@ -57,8 +57,8 @@ public enum EnvPhase {
   AFTER_ALL,
 
   /**
-   * Before each testt in a given test class, {@code EnvProvider}S are instructed to set up their provided
-   * environments by a call to {@code EnvProvider#setUpEnvironment(EnvPhase, Optional<AnnotatedElement>)}.
+   * Before each test in a given test class, {@code EnvProvider}S are instructed to set up their provided
+   * environments by a call to {@code EnvProvider#setUpEnvironment(EnvPhase, Optional)}.
    * <p>
    * It is up to the implementer of the {@code EnvProvider} to decide whether to set up all or only parts of the
    * environment.
@@ -67,12 +67,15 @@ public enum EnvPhase {
 
   /**
    * After each test in a given test class, {@code EnvProvider}S are instructed to tear down their
-   * provided environments by a call to {@code EnvProvider#tearDownEnvironment(EnvPhase, Optional<AnnotatedElement>)}.
+   * provided environments by a call to {@code EnvProvider#tearDownEnvironment(EnvPhase, Optional)}.
    * <p>
    * It is up to the implementer of the {@code EnvProvider} to decide whether to tear down all
    * or only parts of the environment.
    */
   AFTER_EACH,
 
+  /**
+   * This is the phase where instance post processing and method parameter resolution takes place.
+   */
   PREPARE
 }
